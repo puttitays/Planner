4e1c207ad1c08f022bcca8eed2c604b4e7dd3697
puttita <yputtita@gmail.com>
2026-02-06 14:42:40 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+export default function App (){
+return <div><h1>To do list</h1>
+
+
+<div className="d-flex gap-2">
+<input type="text" className= "form-control w-60"  />
+
+<button className="btn btn-dark w-40" ><i className="bi bi-flower2 "></i> </button>
+</div>
+</div>
+
+}
+
+
